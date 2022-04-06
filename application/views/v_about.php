f62be88e77a925ec598b52b7e5db82d80475cab3
Aldi Xanana <marchea01@gmail.com>
2022-04-07 02:02:34 +0700

    Tugas Pertemuan 3
    Membuat template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+<h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+<ul type="disc">
+<li>Nama Depan : Aldi</li>
+<li>Nama Belakang : Xanana</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li> Puri Beta1</li>
+</ul>
+<h4>Tempat Lahir</h4>
+<ul type="none">
+<li>Jakarta</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+<li>Bulutangkis</li>
+<li>Catur</li>
+</ul>
+</section>
