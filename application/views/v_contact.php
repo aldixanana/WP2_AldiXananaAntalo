b2537bd3bd36090b02928b7babbe974b53697d1a
Aldi Xanana <marchea01@gmail.com>
2022-04-14 04:15:27 +0700

    Menambahkan kontak

---
+++ application/views/v_contact.php
@@ -0,0 +1,12 @@
+<section>
+<h1><?php echo $judul ?></h1>
+<h4>Email</h4>
+<ul type="disc">
+<li>aldixanana01@gmail.com</li>
+</ul>
+<br>
+<h4>Telp</h4>
+<ul type="none">
+<li>089619172771</li>
+</ul>
+</section>
